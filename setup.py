cff48eff65cc196a9d0794b7fa914239002381e0
thomas.schneider <dolucky@ya.ru>
2016-03-14 19:12:02 +0300

    asd

--- setup.py
+++ setup.py
@@ -8,10 +8,6 @@ setup(
     packages = ['plugin'],
     license = 'LICENSE',
     zip_safe = False,
-    install_requires = ['cloudify-plugins-common>=3.3'],
-    test_requires = [
-        "cloudify-dsl-parser>=3.4a3"
-        "nose"
-    ]
+    install_requires = ['cloudify-plugins-common>=3.3']
 )
  
