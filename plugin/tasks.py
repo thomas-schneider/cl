33b714d575278176c19f713886052ab70fdb534d
thomas.schneider <dolucky@ya.ru>
2016-03-15 12:41:05 +0300

    asd

--- plugin/tasks.py
+++ plugin/tasks.py
@@ -2,6 +2,6 @@ from cloudify import ctx
 from cloudify.decorators import operation
 
 @operation
-def set_hello(some_property, **kwargs):
+def set_hello(hello, **kwargs):
     # setting node instance runtime property
     ctx.instance.runtime_properties['hello'] = 'world'
